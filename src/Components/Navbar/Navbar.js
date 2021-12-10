707b7e0e8defc0ea77c782ef0c37ca46d1a439bc
ujanmdr <umdr98@gmail.com>
2021-12-10 14:31:07 +0545

    navbar update

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,16 +3,15 @@ import { Link } from "react-router-dom";
 import { auth, logout } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import "./Navbar.css";
+
 function Navbar() {
   const [user] = useAuthState(auth);
   return (
     <>
-      <header className="navbar">
+      <div className="navbar__container">
         {user && (
-          <div>
-            <Link to="/" className="abs">
-              ResourceSearch
-            </Link>
+          <div className="navbar__items">
+            <img className="logo" src="mlogo.png" alt="" />
             <Link to="/" className="abs">
               Home
             </Link>
@@ -31,25 +30,23 @@ function Navbar() {
           </div>
         )}
         {!user && (
-          <div>
-            <Link to="/" className="abs">
-              ResourceSearch
-            </Link>
-            <Link to="/" className="abs">
+          <div className="navbar__items">
+            {/* <img src="mlogo.png" height="100" width="100" alt="Maintainance Nepal" /> */}
+            <div><Link to="/" className="abs">
               Home
             </Link>
-
             <Link to="/about" className="abs">
               About
             </Link>
             <Link to="/contact" className="abs">
               Contact
             </Link>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </div>
+            <Link className="abs" to="/login">Login</Link>
+            <Link className="abs" to="/register">Register</Link>
+            </div>
+          </div> 
         )}
-      </header>
+      </div>
     </>
   );
 }
