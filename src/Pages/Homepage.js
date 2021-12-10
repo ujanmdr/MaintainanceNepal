3b7f749c4374df9cc520d6457a1cd380d6a79bbb
ujanmdr <umdr98@gmail.com>
2021-12-10 23:23:12 +0545

    Update

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -10,34 +10,7 @@ function Homepage() {
   return (
     <div>
       <Navbar /> 
-      {/* <div class="slideshow-container">
-
-        <div class="mySlides fade">
-          <div class="numbertext">1 / 3</div>
-          <img src={slide1} style="width:100%"></img>
-          <div class="text">Caption Text</div>
-        </div>
-
-        <div class="mySlides fade">
-          <div class="numbertext">2 / 3</div>
-          <img src={slide2} style="width:100%"></img>
-          <div class="text">Caption Two</div>
-        </div>
-
-        <div class="mySlides fade">
-          <div class="numbertext">3 / 3</div>
-          <img src={slide3} style="width:100%"></img>
-          <div class="text">Caption Three</div>
-        </div>
-
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-      </div>
-      <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-      </div> */}
+      
     </div>
   );
 }
